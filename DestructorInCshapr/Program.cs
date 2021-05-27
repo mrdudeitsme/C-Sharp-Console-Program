f5ddf06b689b1a30f1b0f9516a885740ea2708e0
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 11:57:23 +0530

    Add files via upload

---
+++ DestructorInCshapr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DestructorInCshapr
+{
+    class person
+    {
+        public string Name;
+        public int Age;
+
+        public person(string Name,int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        public void GetNameAge()
+        {
+            Console.WriteLine("Name is:{0}",Name);
+            Console.WriteLine("Age is:{0}",Age);
+        }
+        ~person()
+        {
+            Console.WriteLine("Destructor is invoke");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            person obj = new person("Sahil",24);
+            obj.GetNameAge();
+            //Console.ReadLine();
+        }
+    }
+}
