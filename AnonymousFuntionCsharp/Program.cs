8f0b767e46643dcdaeb1cfde04a75d1536ab253c
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 11:51:01 +0530

    Add files via upload

---
+++ AnonymousFuntionCsharp/Program.cs
@@ -0,0 +1,45 @@
+﻿// using System;
+// namespace AnonymousFuntionCsharp
+// {
+//     public delegate void MyDelegate(int num);
+//     class Program
+//     {
+      // public static void MyMethod(int a)
+// {
+//     a += 10;
+//     Console.WriteLine(a);
+// }
+//         static void Main(string[] args)
+//         {
+//Anonymous Function
+
+// DeleagateName Refrence Variable = Delegate Keyword (Parameter)
+//             MyDelegate obj = delegate (int a)
+//             {
+//                 a += 10;
+//                 Console.WriteLine(a);
+//             };
+//             obj.Invoke(5);
+//         }
+//     }
+// }
+//----------Also we can send Anonymous Function as a parameter
+
+using System;
+
+namespace AnonymousFuntionCsharp
+{
+    public delegate void MyDelegate(int num);
+    class Program
+    {
+        public static void ShowDelegate(MyDelegate del, int a)
+            {
+                a += 10;
+                del.Invoke(a);  
+            }
+        static void Main(string[] args)
+        {
+            Program.ShowDelegate(delegate(int b){ b += 10; Console.WriteLine (b);},5);
+        }
+    }
+}
---
+++ ArithmaticOperation/Program.cs
@@ -0,0 +1,77 @@
+﻿// using System;
+
+// namespace ArithmaticOperation
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Arithmatic Operators
+
+//             int a=5, b=5,c,d,e,f,g;
+
+//             c = a + b;
+//             d = a - b;
+//             e = a * b;
+//             f = a / b;
+//             g = a % b;
+
+//             Console.WriteLine(c);
+//             Console.WriteLine(d);
+//             Console.WriteLine(e);
+//             Console.WriteLine(f);
+//             Console.WriteLine(g);
+//             Console.ReadLine();
+
+//             Relational or Comparision Operators
+
+//             int a = 10 , b = 25;
+//             bool c  = a == b;
+//             bool d = a != b;
+//             bool e = a < b;
+//             bool f = a > b;
+//             bool g = a <= b;
+//             bool h = a >= b;
+//             Console.WriteLine(c);
+//             Console.WriteLine(d);
+//             Console.WriteLine(e);
+//             Console.WriteLine(f);
+//             Console.WriteLine(g);
+//             Console.WriteLine(h);
+
+//             Assignment Operators
+
+//             int a = 5, b =5 ,c = 2,d = 9 ,e = 10;
+
+//             a += 2;
+//             b -=2;
+//             c *=2;
+//             d /=2;
+//             e %=2;
+//             Console.WriteLine(a);
+//             Console.WriteLine(b);
+//             Console.WriteLine(c);
+//             Console.WriteLine(d);
+//             Console.WriteLine(e);
+
+//             Ternary or Conditional Operators
+
+//             int a = 10;
+
+//             string c = (a<5) ? "a is bigger" : "a is lesser";
+
+//             Console.WriteLine(c);
+
+//             Ternary Operators or conditional Operators
+
+//             int a;
+//             Console.WriteLine("Enter your age");
+//             a = int.Parse(Console.ReadLine());
+
+//             string b = (a>=18) ? "You are eligible to vote" : "You are not eligible to Vote";
+//             Console.WriteLine(b);
+
+            
+//         }
+//     }
+// }
