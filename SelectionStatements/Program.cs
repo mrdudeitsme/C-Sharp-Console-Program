f23edc124528624bf6a6db8f8422b649d890836b
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:15:50 +0530

    Add files via upload

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //IF....Ele
+
+            // int a = 50;
+
+            // if (a > 90)
+            // {
+            //     Console.WriteLine("A is Grater");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("A is smaller");
+            // }
+            // Console.ReadLine();
+
+            //Username & password Checking using if else ststement....
+            //     Console.WriteLine("Enter Your Name");
+            //     string Uname = Console.ReadLine();
+
+            //     Console.WriteLine("Enter Your Password");
+            //     string passwd = Console.ReadLine();
+
+            //     if (Uname == "sahil" && passwd == "sahil@1234")
+            //     {
+            //         Console.WriteLine("Welcome sahil");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Incorrect username or password");
+            //     }
+            //     Console.ReadLine();
+
+
+            //If else if 
+            // Console.WriteLine("Enter Your percentage");
+            // int percentage = int.Parse(Console.ReadLine());
+
+            // if (percentage >= 85)
+            // {
+            //     Console.WriteLine("A Grade");
+            // }
+            // else if (percentage >= 75)
+            // {
+            //     Console.WriteLine("Grade B");
+            // }
+            // else if (percentage >= 60)
+            // {
+            //     Console.WriteLine("Grade C");
+            // }
+            // else if (percentage >= 35)
+            // {
+            //     Console.WriteLine("Grade D");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Fail");
+            // }
+
+
+            //Nested If....
+
+            // Console.WriteLine("Enter your email");
+            // string email = Console.ReadLine();
+
+            // if(email == "sahil@gmail.com")
+            // {
+            //     Console.WriteLine("Enter Your password");
+            //     string password = Console.ReadLine();
+
+            //     if(password == "sahil123")
+            //     {
+            //         Console.WriteLine("Login successfull");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Invalid password");
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Invalid email");
+            // }
+
+            //Switch Case   
+            // int Week_Number = 2;
+
+        //     Console.WriteLine("Enter the week number");
+        //     int Week_Number = int.Parse(Console.ReadLine());
+
+        //     switch (Week_Number)
+        //     {
+        //         case 1:
+        //             Console.WriteLine("Monday");
+        //             break;
+
+        //         case 2:
+        //             Console.WriteLine("Tuesday");
+        //             break;
+
+        //         case 3:
+        //             Console.WriteLine("Wednesday");
+        //             break;
+
+        //         case 4:
+        //             Console.WriteLine("Thursday");
+        //             break;
+
+        //         case 5:
+        //             Console.WriteLine("Friday");
+        //             break;
+
+        //         case 6:
+        //             Console.WriteLine("Saturday");
+        //             break;
+
+        //         case 7:
+        //             Console.WriteLine("Sunday");
+        //             break;
+
+        //             default:
+        //             Console.WriteLine("Invalid Week Number");
+        //             break;
+        //     }
+        // Console.WriteLine("After Switch case statement");
+
+        //Netsted Switch Case
+
+        // Console.WriteLine("Enter Your Pizza Flavor");
+        // string pizza = Console.ReadLine();
+
+        // switch(pizza)
+        // {
+        //     case "fajito":
+        //     Console.WriteLine("You are selected fajito Flavor");
+        //     break;
+
+        //     case "cheese":
+        //     Console.WriteLine("You are selected Cheese Flavor");
+        //     break;
+
+        //     case "primium":
+        //     Console.WriteLine("You are selected Primium Flavor");
+        //     break;
+
+        //     case "Vegetable":
+        //     Console.WriteLine("Enter Vegetable Pizza type");
+        //     string veg = Console.ReadLine();
+
+        //     switch (veg)
+        //     {
+        //         case "onion":
+        //         Console.WriteLine("You are selected Onion type pizza in vegetable flavor");
+        //         break;
+
+        //         case "capsicum":
+        //         Console.WriteLine("You are selected capsicum type pizza in vegetable flavor");
+        //         break;
+
+        //         case "origano":
+        //         Console.WriteLine("You are selected origano in vegetable flavor on the vegetable");
+        //         break;
+
+        //         default:
+        //         Console.WriteLine("Selected type of pizza is not available in vegetable pizza");
+        //         break;
+        //     }
+        //     break;
+            
+        //     default:
+        //     System.Console.WriteLine("Selected Pizza is not available");
+        //     break;
+
+        // }
+
+
+
+        }
+    }
+}
---
+++ StatiAndInstance/Program.cs
@@ -0,0 +1,145 @@
+﻿// using System;
+
+// <<<<<<<<<--------Static & Instance Member Functions------------->>>>>>>>>>>
+
+// namespace StatiAndInstance
+// {
+//     class Student
+//     {
+//         public int RollNo;
+//         public string Fname;
+//         public string Lname;
+//         public string Standerd;
+//         public static string ClgName = "Walington University";
+//         public static int fees = 3000;
+        
+//         public void PrintFullName()
+//         {
+//             string fullname = this.Fname + " " + this.Lname;
+//             Console.WriteLine("Your Full Name is :{0}",fullname);
+//         }
+//         public static int GetFees()
+//         {
+//             return fees;
+//         }
+//         public static int AnnualIncrement(int fee)
+//         {
+//             return fee/10;
+//         }
+//     }
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Student sahil = new Student();
+            
+//             sahil.RollNo = 23;
+//             sahil.Fname = "Sahil";
+//             sahil.Lname = "Dudwadkar";
+//             sahil.Standerd = "MSC";
+
+//             Student siddesh = new Student();
+//             siddesh.RollNo = 24;
+//             siddesh.Fname = "Siddesh";
+//             siddesh.Lname = "Talekar";
+//             siddesh.Standerd = "BSC&Diploma";
+
+//             Console.WriteLine("Student Roll No - :{0}",sahil.RollNo);
+//             Console.WriteLine("First Name - :{0}",sahil.Fname);
+//             Console.WriteLine("Last Name - :{0}",sahil.Lname);
+//             Console.WriteLine("Standard is - :{0}",sahil.Standerd);
+//             sahil.PrintFullName();
+//             Console.WriteLine("College Name is :{0}",Student.ClgName);
+//             Console.WriteLine("Student fees is :{0}",Student.GetFees());
+//             Console.WriteLine("College Anually Increment fees by 10% :{0}",Student.AnnualIncrement(3000));
+            
+
+//             Console.WriteLine("-----------------------------------");
+
+//             Console.WriteLine("Student Roll No - :{0}",siddesh.RollNo);
+//             Console.WriteLine("First Name - :{0}",siddesh.Fname);
+//             Console.WriteLine("Last Name - :{0}",siddesh.Lname);
+//             Console.WriteLine("Standard is - :{0}",siddesh.Standerd);
+//             siddesh.PrintFullName();
+//             Console.WriteLine("College Name is :{0}",Student.ClgName);
+//             Console.WriteLine("Student fees is :{0}",Student.GetFees());
+//             Console.WriteLine("College Anually Increment fees by 10% :{0}",Student.AnnualIncrement(5000));
+
+//         }
+//     }
+// }
+
+//<<<<<<<<<<<<<<<<<<--------------------Static Constructor ------------------>>>>>>>>>>>>>>>
+// using System;
+// namespace StaticConstructorInCsharp
+// {
+//     class Person
+//     {
+//         public static string PersonName;
+//         public static int PersonAge;
+
+//         static Person()
+//         {
+//             PersonName = "Sahil";
+//             PersonAge = 23;
+//             Console.WriteLine("Static Constructor Invokes");
+//         }
+//         public static void GetDetails()
+//         {
+//             Console.WriteLine("Person Name is :{0}",PersonName);
+//             Console.WriteLine("Person Age is :{0}",PersonAge);
+//         }
+//         public Person()
+//         {
+//             Console.WriteLine("Default Constructor invokes");
+//         }
+//     }
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//          Person p = new Person();   
+//         }
+//     }
+    
+// }
+
+//<<<<<<<<<<<<<----------------------------Copy Constructor------------------------>>>>>>>>>>>>
+
+using System;
+namespace StaticConstructorInCsharp
+{
+    class Example
+    {
+        string Name;
+        int Age;
+
+        public Example(string Name,int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        public Example(Example e)
+        {
+            this.Name = e.Name;
+            this.Age = e.Age;
+        }
+        public void GetData()
+        {
+            Console.WriteLine("Name is :{0}",Name);
+            Console.WriteLine("Age is :{0}",Age);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj = new Example("sahil",24);
+            obj.GetData();
+            Example obj1 = new Example(obj);
+            obj1.GetData();
+        }
+    }
+    
+}
