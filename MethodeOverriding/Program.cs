821a994ac709b89640fc1a42d47cac314a10f5ae
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:08:42 +0530

    Add files via upload

---
+++ MethodeOverriding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//Method Methode Overriding with virtual and override method
+
+namespace MethodeOverriding
+{
+    class Parent 
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Parent class method");
+        }
+    }
+    class Child : Parent
+    {
+        public override void show()
+        {
+            Console.WriteLine("Child class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Parent p  = new Child();
+        p.show();
+        }
+    }
+}
---
+++ methfun/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace methfun
+{
+    class Program
+    {
+        //Non-parameterzied method -----------non-static -----instance method----------
+        // public void main()
+        // {
+        //     System.Console.WriteLine("hello");
+        //     System.Console.WriteLine("hii");
+        // }
+        // static void Main(string[] args)
+        // {
+        //     Program p1 = new Program();
+        //     p1.main();
+        // }
+
+        // static method ------- parameterzied method------and send arguments 
+        
+        // public static void ShowResult(int a, int b) //------Parameters
+        // {
+        //     int result =  a + b;
+        //     System.Console.WriteLine("addition is: :" +result);
+        // }
+
+
+
+ //----------------static method parameterzied addition-----------------
+
+
+
+        // static void Main(string[] args)
+        // {
+        //     Console.WriteLine("Enter your first number");
+        //     int num1 = int.Parse(Console.ReadLine());
+
+        //     Console.WriteLine("Enter your Second Number");
+        //     int num2 = int.Parse(Console.ReadLine());
+
+        //     Program.ShowResult(num1,num2); //--------Arguments
+        // }
+
+
+ //----------------static method string parameterzied & arguments-----------------
+
+
+
+        // public static void ShowName(string name = "Unknown") //----Parameters
+        // {
+        //     Console.WriteLine("Your name is :" + name);
+        // }
+
+        // static void Main(string[] args)
+        // {
+        //     Console.WriteLine("Enter your name");
+        //     string result = Console.ReadLine();
+        //     Program.ShowName(result); //-----arguments
+        // }
+
+
+    }
+}
