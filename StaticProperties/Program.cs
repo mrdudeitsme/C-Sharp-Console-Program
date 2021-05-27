6e9bbb9094b9ef2a89f29bec0f37457313d1cfaa
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:18:29 +0530

    Add files via upload

---
+++ StaticProperties/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace StaticProperties
+{
+    class University
+    {
+        private static string UniversityName;
+        private static string DepartmentName;
+
+        public static string _UniversityName //first propert for setting and getting University name 
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Please Enter Correct Name Don't Enter null value");
+                }
+                else
+                {
+                UniversityName = value;
+                }
+            }
+            get
+            {
+                return UniversityName;
+            }
+        }
+        //Second propert for setting and getting University name 
+
+        public static string _DepartmentName
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Enter Correct Department Name");
+                }
+                else
+                {
+                DepartmentName = value;
+                }
+            }
+            get
+            {
+                return DepartmentName;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            University._UniversityName = "Mumbai University";
+            Console.WriteLine("University Name is :{0}",University._UniversityName);
+            University._DepartmentName = "Msc Department";
+            Console.WriteLine("Department Name is :{0}",University._DepartmentName);
+        }
+    }
+}
---
+++ Valuetype_RefrenceType/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Valuetype_RefrenceType
+{
+    //<<<<<<<<<-------------------- Struct - Value type -------- stored on stack memory---------->>>>
+    // struct Employee
+    // {
+    //     public int Salary;
+    //     public int Age;
+    // }
+    // class Program
+    // {
+    //     static void Main(string[] args)
+    //     {
+    //         Employee e = new Employee();
+    //         e.Salary = 5000;
+    //         e.Age = 23;
+
+    //         Employee e1 = e;
+    //         Employee e2 = e;
+
+    //         e.Age = 25;
+
+    //         Console.WriteLine(e.Age);
+    //         Console.WriteLine(e1.Age);
+    //         Console.WriteLine(e2.Age);
+    //<<<<<<<<<-------------------- Class - Refrence type -------- stored on Heap memory---------->>>>
+//     class Employee
+//     {
+//         public int Salary;
+//         public int Age;
+//     }
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Employee e = new Employee();
+//             e.Salary = 5000;
+//             e.Age = 23;
+
+//             Employee e1 = e;
+//             Employee e2 = e;
+
+//             e.Age = 25;
+
+//             Console.WriteLine(e.Age);
+//             Console.WriteLine(e1.Age);
+//             Console.WriteLine(e2.Age);
+//         }
+//     }
+// }
+
