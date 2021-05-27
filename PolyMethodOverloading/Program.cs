dfe23122c89fb18c1582dc92968a3642f2331d02
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:13:20 +0530

    Add files via upload

---
+++ PolyMethodOverloading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PolyMethodOverloading
+{
+    // Polymorphism Method Overloading --- Same Function or Method but different signature or parameters  
+    class Addition 
+    {
+        public void Add()
+        {
+           int a = 20;
+           int b = 30;
+           int c = a + b;
+           Console.WriteLine(c);
+        }
+        public void Add(int a , int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+        public void Add(string a, string b)
+        {
+            string c = a + " " + b;
+            Console.WriteLine(c);
+        }
+        public void Add(float a , float b)
+        {
+            float c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition p = new Addition();  //And access those method using object instatiation 
+            p.Add(2.5f,3.25f);
+        }
+    }
+}
---
+++ Ref_Out/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ref_Out
+{
+    class Program
+    {   
+        //Pass By Value
+        // public static void PassByValue(int a)
+        // {
+        //     a = a+10;
+        //     System.Console.WriteLine("Value is :{0}", a);
+        // }
+        // static void Main(string[] args)
+        // {
+        //     int value = 5;
+        //     PassByValue (value);
+        //     Console.WriteLine(value);
+        // }
+
+        //Pass By Refrence
+        // public static void PassByRef(ref int a)
+        // {
+        //     a = a + 10;
+        //     System.Console.WriteLine("Value is:{0}",a);
+        // }
+        // static void Main(string[] args)
+        // {
+        //     int value = 5;
+        //     PassByRef(ref value);
+        //     System.Console.WriteLine(value);
+        // }
+        //Pass by out Keyword
+        // public static void PassByOut(out int a)
+        // {
+        //     a = 20;
+        //     System.Console.WriteLine("Value is :{0}", a);
+        // }
+        // static void Main(string[] args)
+        // {
+        //     int value;
+        //     PassByOut(out value);
+        //     System.Console.WriteLine(value);
+        // }
+    }
+}
