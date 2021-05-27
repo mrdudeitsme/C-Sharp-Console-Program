5dcd599fcb7e4b149ee276db49a2452786306d3d
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:10:05 +0530

    Add files via upload

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodHiding
+{
+    class Parent
+    {
+        public void show()
+        {
+            Console.WriteLine("Parent class method");
+        }
+    }
+    class Child : Parent
+    {
+        public new void show() //if hiding was intended then used "new" keyword in derived / child class then u will not get warning but your program will run correctly..
+        {
+            //base.show(); //1st way to hide child class method and invoke parent class method by using "base" keyword....
+
+            Console.WriteLine("Child class Method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Child(); // instantiate object with new keyword and child class constructor...
+            p.show();
+        //Child cd = new Child();
+         //cd.show();   
+         // ((Parent)cd).show(); //2nd way for invoking parent class method and hide child class method --- casting parent to child and hide the method of child class.
+        }
+    }
+}
---
+++ MethodHiding2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MethodHidings
+{
+    class Employee
+    {
+        public string fName;
+        public string lName;
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(fName +  " " + lName);
+        }
+    }
+    class PartTimeEmp : Employee
+    {
+        public new void PrintFullName()
+        {
+            //base.PrintFullName();
+            Console.WriteLine(fName + " " + lName + " PTE");
+        }
+
+    }
+    class FullTimeEmp : Employee
+    {
+        public new void PrintFullName()
+        {
+            Console.WriteLine(fName + " " + lName + "FTE");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Employee Emp = new PartTimeEmp();
+
+            PartTimeEmp PTE = new PartTimeEmp();
+            PTE.fName = "Sahil";
+            PTE.lName = "Dudwadkar";
+            // Emp.fName = "Sahil";
+            // Emp.lName = "Dudwadkar";
+            // Emp.PrintFullName();
+
+            //((Employee)PTE).PrintFullName();
+            PTE.PrintFullName();
+        }
+    }
+}
