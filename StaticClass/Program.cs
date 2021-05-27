61a4681fe726fe994ed0e8f241a604e7ca94e786
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:17:31 +0530

    Add files via upload

---
+++ StaticClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StaticClass
+{
+    static class Product 
+    {
+        public static int ProductID;
+        public static string ProductName;
+        public static int ProductPrice;
+
+        static Product()
+        {
+            ProductID = 101;
+            ProductName = "Whiteboard";
+            ProductPrice = 2000;
+        }
+        
+        public static void GetProducts()
+        {
+            Console.WriteLine("Product ID is :{0}",ProductID);
+            Console.WriteLine("Product Name is :{0}",ProductName);
+            Console.WriteLine("Product Price is :{0}",ProductPrice);
+        }
+        public static void GetDiscount()
+        {
+            int d_amount = ProductPrice / 10;
+            Console.WriteLine("Discount on product is 10% :{0}",d_amount);
+            Console.WriteLine("Total Amount After discount :{0}",ProductPrice - d_amount);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product.GetProducts();
+            Product.GetDiscount();
+        }
+    }
+}
---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    class Person
+    {
+        public static string PersonName; 
+        public static int PersonAge;
+
+        static Person()         //Static Constructor Intiatiate without access modifier
+        {
+            PersonName = "Sahil";
+            PersonAge = 20;
+            Console.WriteLine("Static constructor Invoked !!");
+        }
+        public Person()         //Default Constructor
+        {
+            Console.WriteLine("Default Constructor Invoked !!");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Sahil = new Person(); // Default Constructor for first Object
+            Person Sidd = new Person();  // Default Constructor for second Object
+        }
+    }
+}
