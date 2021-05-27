94734934fc8eadf7578051250ec3ce59a4327508
Sahil Shripad Dudwadkar <36694162+mrdudeitsme@users.noreply.github.com>
2021-05-27 12:14:28 +0530

    Add files via upload

---
+++ RestartProgram/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace RestartProgram
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+// -------------------- Using do...while loop--------------//
+
+            string confirm;
+            do
+            {
+                Console.WriteLine("Enter a number");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter a number");
+                int num2 = int.Parse(Console.ReadLine());
+
+                int add = num1 + num2;
+                Console.WriteLine("Your addition is :" + add);
+
+                Console.WriteLine("Do you wants to repeat ur program");
+                confirm = Console.ReadLine().ToLower();
+            }
+            while(confirm == "yes");
+
+// -------------------using while loop--------------------//
+
+//             string confirm;
+//             while(true)
+//             {
+//                 Console.WriteLine("Enter a number");
+//                 int num1 = int.Parse(Console.ReadLine());
+
+//                 Console.WriteLine("Enter a number");
+//                 int num2 = int.Parse(Console.ReadLine());
+
+//                 int add = num1 + num2;
+//                 Console.WriteLine("Your addition is :" + add);
+
+//                 Console.WriteLine("Do you wants to repeat ur program");
+//                 confirm = Console.ReadLine().ToLower();
+
+//                 if(confirm == "yes")
+//                 {
+//                     continue;
+//                 }
+//                 else
+//                 {
+//                     break;
+//                 }
+//             }
+
+
+        }
+    }
+}
---
+++ SealedClass/Program.cs
@@ -0,0 +1,67 @@
+﻿//Sealed class
+
+// using System;
+
+// namespace SealedClass
+// {
+//     class parent
+//     {
+//         public void show1()
+//         {
+//             Console.WriteLine("Parent Method");
+//         }
+//     }
+//     class derived : parent
+//     {
+//         public void show2()
+//         {
+//             Console.WriteLine("Child Method");
+//         }
+//     }
+        
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             derived dc = new derived();
+//             dc.show1();
+//         }
+//     }
+// }
+
+//Sealed Method 
+
+using System;
+
+namespace Sealed 
+{
+    class A
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Parent Class Method");
+        }
+    }
+    class B : A
+    {
+        public sealed override void show() // we cannot override inherited member because it is sealed...
+        {
+            Console.WriteLine("Child class method");
+        }
+    }
+    class C : B
+    {
+        public override void show()
+        {
+            Console.WriteLine("2nd Child class Method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C dd = new C();
+            dd.show();
+        }
+    }
+}
